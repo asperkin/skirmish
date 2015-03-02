6c4e867c803a23ceb36c81f6ac06e4d5614e500c
asperkin <aspsc2@gmail.com>
2015-03-01 22:24:26 -0800

    Updated win condition, Gallop now 10 mana on a 20 second cooldown

--- scripts/vscripts/control_point.lua
+++ scripts/vscripts/control_point.lua
@@ -34,7 +34,7 @@ function capture_check(event)
    then
       --if only one players units are in circle
       cappingTeam = SinglePlayerOnPoint(units_on_point)
-      if cappingTeam ~= caster:GetTeam() -- actual contention
+      if cappingTeam ~= caster:GetTeam() and cappingTeam ~= nil-- actual contention
       then
          if caster.contested == 0 or caster.contested == nil -- beginning of contention
          then
@@ -56,6 +56,7 @@ function capture_check(event)
                then
                   caster:SetControllableByPlayer(0, true)
                   goodguy_points = goodguy_points + 1
+                  print("goodguy_points=" .. goodguy_points)
                   if prevTeam == DOTA_TEAM_BADGUYS 
                   then badguy_points = badguy_points - 1 end
                   if goodguy_points == num_points
@@ -66,6 +67,7 @@ function capture_check(event)
                then
                   caster:SetControllableByPlayer(5, true)
                   badguy_points = badguy_points + 1
+                  print("badguy_points=" .. badguy_points)
                   if prevTeam == DOTA_TEAM_GOODGUYS
                   then goodguy_points = goodguy_points - 1 end
                   if badguy_points == num_points
